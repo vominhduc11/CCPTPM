b3c8886c88b191a1400fa1a04041c8ed91b3d6a8
vomin <vominhduc760@gmail.com>
2025-02-18 15:26:33 +0700

    update

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    { ignores: ['dist', 'vite.config.ts'] },
+    { ignores: ['dist', 'vite.config.ts', 'eslint.config.js'] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ['**/*.{ts,tsx}'],
